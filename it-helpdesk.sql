e47df80df44d2261ab9e28f47bb63a25d9ef5335
malhajri20222 <malhajri20222@gmail.com>
2026-07-23 16:33:00 +0300

    Add files via upload

---
+++ it-helpdesk.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- مضيف: localhost:8889
+-- وقت الجيل: 23 يوليو 2026 الساعة 13:30
+-- إصدار الخادم: 8.0.44
+-- نسخة PHP: 8.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- قاعدة بيانات: `it-helpdesk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` int NOT NULL,
+  `department_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `departments`
+--
+
+INSERT INTO `departments` (`id`, `department_name`) VALUES
+(1, 'IT'),
+(2, 'HR'),
+(3, 'Finance'),
+(4, 'Sales'),
+(5, 'Administration');
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `issue_types`
+--
+
+CREATE TABLE `issue_types` (
+  `id` int NOT NULL,
+  `issue_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `issue_types`
+--
+
+INSERT INTO `issue_types` (`id`, `issue_name`) VALUES
+(1, 'Hardware'),
+(2, 'Software'),
+(3, 'Network'),
+(4, 'Printer'),
+(5, 'Email'),
+(6, 'Password Reset'),
+(7, 'Internet'),
+(8, 'VPN'),
+(9, 'Microsoft Office'),
+(10, 'Shared Folder'),
+(11, 'New User Account');
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id` int NOT NULL,
+  `employee_id` varchar(20) NOT NULL,
+  `department` varchar(100) NOT NULL,
+  `issue_type` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `priority` varchar(20) NOT NULL,
+  `status` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'open',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `employee_id`, `department`, `issue_type`, `description`, `priority`, `status`, `created_at`) VALUES
+(32, 'EMP001', 'HR', 'Printer', 'Finance office printer is displaying a paper jam error even after clearing it.\r\n', 'High', 'open', '2026-07-23 11:53:31'),
+(33, 'EMP002', 'HR', 'Other', 'Unable to log in to the HR system after password expired.\r\n', 'Low', 'open', '2026-07-23 11:54:04'),
+(34, 'EMP003', 'IT', 'Network', 'No internet connection in the entire IT department.\r\n', 'Medium', 'open', '2026-07-23 11:54:33'),
+(35, 'EMP004', 'Marketing', 'Email', 'Outlook cannot send or receive emails since this morning.\r\n', 'High', 'In Progress', '2026-07-23 11:55:05'),
+(36, 'EMP011', 'Finance', 'Software', 'Accounting software fails to launch after the latest update.\r\n', 'High', 'Closed', '2026-07-23 11:55:42');
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','support') DEFAULT 'admin',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `username`, `password`, `role`, `created_at`) VALUES
+(1, 'admin', 'admin', '$2y$10$D/JSOUClnv.YgFtFXBwnb.4yA4bu0214UbjdYBF3rJ3egGZ0vswfG', 'admin', '2026-07-23 12:16:47'),
+(2, 'Ahmed Alqahtani', 'ahmed.it', '$2y$10$3RQK0C3nW/TPfaAwCL191e7QY3y6zYcDeWBEWWX7yByWfDSKqzmmG', 'support', '2026-07-23 12:19:08'),
+(3, 'Sarah Alharbi', 'sarah.hr', '$2y$10$JUjI5eq88U7bI2f1pEQKKunSNKKrXwU9UDxAiYbl7C/7GtYF6XlJ2', 'support', '2026-07-23 12:20:21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- فهارس للجدول `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- فهارس للجدول `issue_types`
+--
+ALTER TABLE `issue_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- فهارس للجدول `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- فهارس للجدول `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `issue_types`
+--
+ALTER TABLE `issue_types`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
